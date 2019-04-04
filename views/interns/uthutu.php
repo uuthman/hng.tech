2e7b8a92b539460c877e40375a95f50f50b3bebf
Ayinde Uthman <uthmanayinde6@gmail.com>
2019-04-04 18:07:11 +0100

    added my profile

---
+++ views/interns/uthutu.php
@@ -0,0 +1,39 @@
+<html>
+<?php include 'partials/header.php';?>
+<body class=" pi">
+<?php include 'partials/navbar.php';?>
+<section class="main-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-sm-6 col-xs-12">
+                <div class="col-md-6 col-xs-12">
+                    <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/de42yadcm/image/upload/v1554396433/IMG-5536.jpg" alt="Mark Essien">
+                        <h6>Ayinde Uthman</h6>
+                        <p>Backend Developer</p>
+                        <ul class="list">
+                            <li><a href="https://twitter.com/uthu_tu" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                            <li><a href="https://github.com/uuthman" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                            <li><a href="https://medium.com/@adamichelllle" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xs-12 align-items-center">
+                    <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>
+                           I am a backend developer(Android & Web), creating solutions with my backend skills has been great inspiration for me to do more and advance in the tech world
+                        </p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>Java | PHP | Golang</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+</body>
+<?php include 'partials/footer.php';?>
+</html>
